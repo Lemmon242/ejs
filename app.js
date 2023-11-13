60365f497fe3fb171da03aaa00223878c6107009
Lemmon242 <stephenokeke1354@gmail.com>
2023-11-13 12:47:27 +0100

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 5050;
-const morgan = require('morgan');
-const mongoose = require ('mongoose');
-require('dotenv/config')
-
-
-const mongoDBUrl = process.env.DBURL
-
-
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const connect = require("./db/mongoDB");
+require("dotenv/config");
 
 // custom middlewears
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 // app.use((req,res,next)=>{
 //     console.log('new request made');
 //     console.log('host:', req.hostname);
@@ -20,49 +16,58 @@ app.set('view engine', 'ejs')
 //     next()
 // })
 
-app.use(morgan('dev'))
-app.use(express.static('public'))
-
-
-
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 // routes
 // app.get("/", (req, res) => {
 // res.send("Welcome home")
 
 // })
-const tasks =[
-    {name:'Halimat',title:'halimats clothing', task:'client deliveries this morning'},
-    {name:'Chimelu',title:'I.T experience', task:'to give my instructor my log book'},
-    {name:'Leewhy',title:'Leewhy Concept', task:'Trading and general contracts'},
-]
-app.get('/',(req,res)=>{
-    res.render('index', {title:'Home || Page', tasks})
-})
+const tasks = [
+  {
+    name: "Halimat",
+    title: "halimats clothing",
+    task: "client deliveries this morning",
+  },
+  {
+    name: "Chimelu",
+    title: "I.T experience",
+    task: "to give my instructor my log book",
+  },
+  {
+    name: "Leewhy",
+    title: "Leewhy Concept",
+    task: "Trading and general contracts",
+  },
+];
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home || Page", tasks });
+});
 
-app.get('/about',(reg,res)=>{
-    res.render('about', {title:'About || Page'})
-})
+app.get("/about", (reg, res) => {
+  res.render("about", { title: "About || Page" });
+});
 
-app.get('/tasks', (req,res)=>{
-    res.render('tasks', {title:'Task || Page'})
-})
+app.get("/tasks", (req, res) => {
+  res.render("tasks", { title: "Task || Page" });
+});
 
-app.use((req,res)=>{
-    res.render('404', {title:'404 || Page'})
-})
-
+app.use((req, res) => {
+  res.render("404", { title: "404 || Page" });
+});
 
 // db connection
-mongoose.connect(mongoDBUrl)
-.then(() => console.log('connected successfully'));
-
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server connected to http://localhost:${PORT}`);
-
-})
-
-
+connect()
+  .then(() => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`Server connected to http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log("cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("invalid database connection...!", error);
+  });
