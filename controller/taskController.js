232af3c5b005efe271be44d417e903e235b18d73
Lemon242 <stephenokeke1354@gmail.com>
2023-11-20 12:02:46 +0100

    first message

--- controller/taskController.js
+++ controller/taskController.js
@@ -22,7 +22,7 @@ const create_task = async(req,res)=>{
     }catch(error){
         console.log(error);
     }
-  }
+  };
 
   const single_task = async(req,res)=>{
     const id = req.params.id
@@ -31,6 +31,8 @@ const create_task = async(req,res)=>{
       // const data = await TASKS.findByIdAndDelete({_id : id})
       const result = await TASKS.findById(id)
       res.status(200).render('singlepage' , {title:'single || page',task:result})
+
+
       
   
   
@@ -38,10 +40,55 @@ const create_task = async(req,res)=>{
       console.log(error);
     }
   
+  };
+
+  // update route
+  const edit_task = async(req,res)=>{
+    const id = req.params.id;
+    const updatedData = req.body;
+    try{
+      await TASKS.findByIdAndUpdate(id, updatedData, {new:true})
+      res.status(301).redirect('/')
+    }catch(error){
+      console.log(error);
+    }
   }
+
+
+  // edit pags
+
+  const edit_page = async (req,res)=>{
+    const id = req.params.id
+    try{
+      const result = await TASKS.findById(id)
+      res.render('editpage',{title: 'edit || page', task:result})
+  }catch(error){
+    console.log(error);
+  }
+
+}
+
+  
+   
+   
+
+
+
+    
+                
+    
+    
+    
+
+   
+    
+    
+
 
   module.exports = {
     create_task,
     delete_task,
-    single_task
+    single_task,
+    edit_page,
+    edit_task
   }
--- router/taskRouter.js
+++ router/taskRouter.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const TASKS = require('../model/taskModel');
-const { create_task, delete_task, single_task } = require('../controller/taskController');
+const { create_task, delete_task, single_task, edit_task, edit_page } = require('../controller/taskController');
 
 // post route c -- create
 router.post('/create', create_task);
   
   // route params for single page
-  router.get('/route/:id',single_task)
+  router.get('/route/:id',single_task);
   
   // delete route D -- delete
-  router.get('/delete/:id',delete_task)
+  router.get('/delete/:id',delete_task);
+
+  // edit route E -- edit
+  router.get('/editpage/:id', edit_page);
+
+  // update route
+  router.post('/edit/:id', edit_task)
+
+
+
+
+  
   
 //    // edit
   //  app.get('/api/v1/edit/:id',async (req,res)=>{
